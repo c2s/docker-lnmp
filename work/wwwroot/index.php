69235ace2e280ba0035edc95ccd0bcd642f296b9
mofei <linux.root.users@gmail.com>
2018-04-27 16:28:40 +0800

    init

---
+++ work/wwwroot/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Docker LNMP</title>
+	<style type="text/css">
+		html{
+			font-family:sans-serif;-ms-text-size-adjust:100%;-webkit-text-size-adjust:100%
+		}
+		.mine{
+			font-size:30px;margin-top:200px;
+			color:#333;
+		}
+		.desc{
+			text-align: center;
+			color:#ccc;
+			margin-bottom: 50px;
+			line-height: 30px;
+		}
+		footer{
+			text-align: center;
+		}
+		footer a{
+			color:#fff;
+			background: #F95445;
+			display: block;
+			width: 180px;
+			margin:0 auto;
+			text-decoration: none;
+			line-height: 40px;
+			height: 40px;
+			border-radius: 3px;
+		}
+	</style>
+</head>
+<body>
+
+<p align="center" class="mine">Docker LNMP</p>
+
+<p class="desc">
+	Version：2.0<br />
+	Time：<?= date_default_timezone_get() . "&nbsp;/&nbsp;" . date("Y-m-d H:i:s");?>
+</p>
+
+<footer>
+	<a href="https://github.com/menghuanplus/docker-lnmp" target="_blank">Get Documentation</a>
+
+
+</footer>
+
+</body>
+</html>
